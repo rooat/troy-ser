99076fae5ad834af721d6dbf04da70c7fb1acb16
rooat <welling1234@gmail.com>
2019-07-05 18:32:53 +0800

    u

--- api/users/logOut.js
+++ api/users/logOut.js
@@ -12,18 +12,14 @@ logOut = async (req, res, next) => {
 			lan = global.lan;
 		}
 		
-		//let sessionId = config.utils.md5(JSON.stringify(req.cookies))
-		let keysuser = await config.getAsync(token);
-		if(!keysuser){
-			return res.send(config.utils.result_req(-2,"10011",config.tips[lan].LOGIN_REPEAT))
-		}
-
-		let keys = config.utils.md5(keysuser);
-		if(token==keys){
-			//let sessionId = config.utils.md5(JSON.stringify(req.cookies))
-			await config.expireAsync(token,0);
-			//await config.expireAsync(sessionId,0)
-			return res.send(config.utils.result_req(0,"10010",config.tips[lan].LOGOUT_SUCCESS))
+		if(keysuser){
+			let keys = config.utils.md5(keysuser);
+			if(token==keys){
+				//let sessionId = config.utils.md5(JSON.stringify(req.cookies))
+				await config.expireAsync(token,0);
+				//await config.expireAsync(sessionId,0)
+				return res.send(config.utils.result_req(0,"10010",config.tips[lan].LOGOUT_SUCCESS))
+			}
 		}
 		return res.send(config.utils.result_req(-2,"10011",config.tips[lan].TOKEN_INVALID));
 			
