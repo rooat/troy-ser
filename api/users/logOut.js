31d78ae63459ef6bf745389bf45c591b09760dab
rooat <welling1234@gmail.com>
2019-07-05 19:27:48 +0800

    u

--- api/users/logOut.js
+++ api/users/logOut.js
@@ -11,9 +11,12 @@ logOut = async (req, res, next) => {
 		if(!lan){
 			lan = global.lan;
 		}
+		console.log("token---",token)
 		let keysuser = config.getAsync(token);
+		console.log("keysuser::::",keysuser)
 		if(keysuser){
 			let keys = config.utils.md5(keysuser);
+			console.log("keys----",keys)
 			if(token==keys){
 				//let sessionId = config.utils.md5(JSON.stringify(req.cookies))
 				await config.expireAsync(token,0);
