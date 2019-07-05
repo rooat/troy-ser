960c735d7b7fde87f1ab6c6383751fe951a418e5
rooat <welling1234@gmail.com>
2019-07-05 20:58:52 +0800

    u

--- config.js
+++ config.js
@@ -452,9 +452,9 @@ const web3 = new Web3(new Web3.providers.HttpProvider('http://etzrpc.org'));
 
  global.lan=1;
 
-var benefitTimeCal = 60000;//收益结算周期
-var nodeTimeCal = 60000;//节点结算周期
-var teamTimeCal = 60000;//团队汇总周期
+var benefitTimeCal = 600000;//收益结算周期
+var nodeTimeCal = 600000;//节点结算周期
+var teamTimeCal = 600000;//团队汇总周期
 
 var utils = require('./utils/utils')
 
