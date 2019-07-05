ee50c60d4110d428c0cd9bdf8190b3d5201d955c
rooat <welling1234@gmail.com>
2019-07-05 19:41:32 +0800

    u

--- class/BenefitClass.js
+++ class/BenefitClass.js
@@ -10,12 +10,12 @@ class BenefitClass{
 		let that = this;
 		setInterval(async function(){
 			console.log("=========benefit-------")
-		      if(new Date().getTime()>that.nextTime){
+		    //  if(new Date().getTime()>that.nextTime){
 		      	let zc = await config.priceData.findOne({where:{symbol:"ZC"}})
 				that.zcprice = zc.price_usd
 		        that.loopBenefit();
 		        that.nextTime = new Date(config.utils.nextTimeFormat()).getTime();
-		      }
+		    //  }
 		},config.benefitTimeCal)
 		
 	}
